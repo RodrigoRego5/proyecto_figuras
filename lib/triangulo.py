62081f5c1ba9762e00b8162ebaf648cccc8a143e
Rodrigetoo <rodrigolopezrego@gmail.com>
2024-09-24 18:00:43 +0200

    Creadas las funciones del triángulo

---
+++ lib/triangulo.py
@@ -0,0 +1,12 @@
+lado1= int
+lado2=int
+lado3=int
+h = int
+def get_identificador() -> str:
+    return "triángulo"
+
+def get_area(lado1,h):
+    return lado1*h/2
+
+def get_perimetro(lado1,lado2,lado3):
+  return lado1 + lado2 + lado3 

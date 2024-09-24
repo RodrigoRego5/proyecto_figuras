28ebe7396e465b0b0544ae40a8c434d1e1c1b70a
Rodrigetoo <rodrigolopezrego@gmail.com>
2024-09-24 18:31:51 +0200

    Agregadas nuevas funciones al main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from lib import cuadrado
+from lib import triangulo
 print("Proyecto Figuras")
 lado = 4
 print(f"El área de un  cuadrado de lado {lado} es : {cuadrado.get_area(lado)}")
 
+lado1=5
+lado2 =3
+lado3=4
+h=2
+print(f"El área de un  triángulo de lado {lado1,h} es : {triangulo.get_area(lado1,h)}")
